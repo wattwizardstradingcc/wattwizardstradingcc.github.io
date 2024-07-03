a11f78bdb39bce820650f8434134b6e40251cc48
wattwizardstradingcc <jesseeneas2002@gmail.com>
2024-07-03 21:08:20 +0200

    webs

--- js/script.js
+++ js/script.js
@@ -4,20 +4,16 @@ document.addEventListener('DOMContentLoaded', function() {
     const prevBtn = document.getElementById('prevBtn');
     const nextBtn = document.getElementById('nextBtn');
     const cardWidth = cards[0].offsetWidth; // Assuming all cards have the same width
-    const numCards = cards.length;
     let currentIndex = 0;
-
-    slider.style.width = `${cardWidth * numCards}px`; // Set slider width to fit all cards
 
     function showSlide(index) {
         if (index < 0) {
             index = 0;
-        } else if (index >= numCards) {
-            index = numCards - 1;
+        } else if (index >= cards.length) {
+            index = cards.length - 1;
         }
         currentIndex = index;
-        const offset = -currentIndex * cardWidth;
-        slider.style.transform = `translateX(${offset}px)`;
+        slider.style.transform = `translateX(${-currentIndex * cardWidth}px)`;
     }
 
     prevBtn.addEventListener('click', function() {
